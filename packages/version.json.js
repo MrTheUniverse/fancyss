3b26aa5132cb6bbb910454e7fb263fd677764ed3
hq450 <qhuang450@gmail.com>
2025-02-11 00:19:06 +0800

    add ipq device support

--- packages/version.json.js
+++ packages/version.json.js
@@ -1,14 +1,16 @@
 {
   "name": "fancyss",
-  "version": "3.3.6",
-  "md5_hnd_full": "ffdbd434c0efe5e2f28e04ae3f24590c",
-  "md5_hnd_lite": "b5dba44272f08cdb5668807cc7228e62",
-  "md5_hnd_v8_full": "35ad3b45c3b3d522f36a1cb44a4706d6",
-  "md5_hnd_v8_lite": "87fdf83f4a1834efb5b571f21417446c",
-  "md5_qca_full": "be20fa416c40f768159a36c537d18934",
-  "md5_qca_lite": "d6490b856861fb2ebd2f104a5ac28e0d",
-  "md5_arm_full": "72726bf27dda2ee1929790ce57f2c3f7",
-  "md5_arm_lite": "5b4bd0db232c2700a10dee2bae05d9d4",
-  "md5_mtk_full": "cc50618c25b4ef23f5ae3d3d12528cac",
-  "md5_mtk_lite": "f6a4f8edb73122b9426f15a1c71c03dc"
+  "version": "3.3.7",
+  "md5_hnd_full": "4aab9dd28e1eba5debbf9cad7b58e0be",
+  "md5_hnd_lite": "178dd40d8980868400560e8950496a3d",
+  "md5_hnd_v8_full": "62351a101acd745acec00e75612779be",
+  "md5_hnd_v8_lite": "c2770b701266d66fdf073514fae29474",
+  "md5_qca_full": "af5ed6755b791ff77f8fb5f67e90b652",
+  "md5_qca_lite": "9e21a3ae238127379526512a089dc811",
+  "md5_arm_full": "95a93a6eebd689902327146cd740569d",
+  "md5_arm_lite": "ac9cf02c5d8984655cc450f683286b12",
+  "md5_mtk_full": "16415bc8aba3e56f0d355c29c848a0f3",
+  "md5_mtk_lite": "23ce53d9745045c6e7f9b83ca6871be2",
+  "md5_ipq_full": "be89726b8805869c2068be46bdaee1bb",
+  "md5_ipq_lite": "43e8185748d1743b46002468518fa8e0"
 }
